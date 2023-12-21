af32d87081b689eb6c195a16290678c887aead9d
Mauricio Dolinski <mauriciompd@gmail.com>
2023-12-21 09:46:44 -0300

    leet 383

--- Leetcode/383. Ransom Note.java
+++ Leetcode/383. Ransom Note.java
@@ -1,15 +1,15 @@
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
-        Map<Character, Integer> charCounts = new HashMap<>();
+        Map<Character, Integer> mapMagazineChar = new HashMap<>();
         for (char c : magazine.toCharArray()) {
-            charCounts.put(c, charCounts.getOrDefault(c, 0) + 1);
+            mapMagazineChar.put(c, mapMagazineChar.getOrDefault(c, 0) + 1);
         }
 
         for (char c : ransomNote.toCharArray()) {
-            if (!charCounts.containsKey(c) || charCounts.get(c) == 0) {
+            if (!mapMagazineChar.containsKey(c) || mapMagazineChar.get(c) == 0) {
                 return false;
             }
-            charCounts.put(c, charCounts.get(c) - 1);
+            mapMagazineChar.put(c, mapMagazineChar.get(c) - 1);
         }
 
         return true;
