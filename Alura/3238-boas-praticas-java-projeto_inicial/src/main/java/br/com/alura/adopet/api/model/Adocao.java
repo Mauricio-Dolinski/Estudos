986f1ecd46dfc5503dcda0ef9c7c23ae914f0d62
Mauricio Dolinski <mauriciompd@gmail.com>
2023-12-26 08:49:13 -0300

    Alura: Removendo redundancias

--- Alura/3238-boas-praticas-java-projeto_inicial/src/main/java/br/com/alura/adopet/api/model/Adocao.java
+++ Alura/3238-boas-praticas-java-projeto_inicial/src/main/java/br/com/alura/adopet/api/model/Adocao.java
@@ -15,30 +15,21 @@ public class Adocao {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id")
     private Long id;
 
-    @Column(name = "data")
     private LocalDateTime data;
 
     @ManyToOne
-    @JsonBackReference("tutor_adocoes")
-    @JoinColumn(name = "tutor_id")
     private Tutor tutor;
 
     @OneToOne
-    @JoinColumn(name = "pet_id")
-    @JsonManagedReference("adocao_pets")
     private Pet pet;
 
-    @Column(name = "motivo")
     private String motivo;
 
     @Enumerated(EnumType.STRING)
-    @Column(name = "status")
     private StatusAdocao status;
 
-    @Column(name = "justificativa_status")
     private String justificativaStatus;
 
     @Override
