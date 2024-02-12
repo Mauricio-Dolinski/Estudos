69b3d36011d7ac80f3a896bb320c4a69d11d950c
Mauricio Dolinski <mauriciompd@gmail.com>
2024-02-12 09:54:54 -0300

    Leet: 217

--- Leetcode/217. Contains Duplicate.java
+++ Leetcode/217. Contains Duplicate.java
@@ -1,16 +1,11 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        HashSet<Integer> set = new HashSet<Integer>();
-
-        for (int i = 0; i < nums.length; i++){
-            if (set.contains(nums[i])){
-                return true;
-            }
-            else{
-                set.add(nums[i]);
-            }
+        HashSet<Integer> set = new HashSet<>();
+        
+        for (int value : nums)
+        {
+            if (!set.add(value)) return true;
         }
-
         return false;
     }
 }
