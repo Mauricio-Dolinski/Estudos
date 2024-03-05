694f0b160b7bc0f613dde440dcbec7012cb41381
Mauricio Dolinski <mauriciompd@gmail.com>
2024-03-05 10:38:08 -0300

    HackerRank Java Strings

---
+++ HackerRank/java/strings/Java Regex.java
@@ -0,0 +1,24 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.Scanner;
+
+class Solution{
+
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        while(in.hasNext()){
+            String IP = in.next();
+            System.out.println(IP.matches(new MyRegex().pattern));
+        }
+
+    }
+}
+
+//Write your code here
+class MyRegex {
+    String pattern;
+    
+    public MyRegex(){
+        this.pattern = "(\\b25[0-5]|\\b2[0-4][0-9]|\\b[01]?[0-9][0-9]?)(\\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}";
+    }
+}
