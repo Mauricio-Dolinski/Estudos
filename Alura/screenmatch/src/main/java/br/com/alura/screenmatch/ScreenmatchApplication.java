61a22991ee2017f87ebd0ff633d6dedda60fa504
Mauricio Dolinski <mauriciompd@gmail.com>
2024-06-05 17:13:56 -0300

    Alura Spring aula 1.10

--- Alura/screenmatch/src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
+++ Alura/screenmatch/src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -4,7 +4,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import br.com.alura.screenmatch.model.DadosSerie;
 import br.com.alura.screenmatch.service.ConsumoAPI;
+import br.com.alura.screenmatch.service.ConverteDados;
 
 @SpringBootApplication
 public class ScreenmatchApplication implements CommandLineRunner {
@@ -18,8 +20,11 @@ public class ScreenmatchApplication implements CommandLineRunner {
 		var consumoAPI = new ConsumoAPI();
 		var json = consumoAPI.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=3a812e74");
 		System.out.println(json);
+		ConverteDados conversor = new ConverteDados();
+		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
+		System.out.println(dados);
 
-		
+
 	}
 
 }
---
+++ Alura/screenmatch/src/main/java/br/com/alura/screenmatch/service/ConverteDados.java
@@ -0,0 +1,25 @@
+package br.com.alura.screenmatch.service;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class ConverteDados implements IConverteDados{
+    private ObjectMapper mapper = new ObjectMapper();
+
+    @Override
+    public <T> T obterDados(String json, Class<T> classe) {
+        try {
+            return mapper.readValue(json, classe);
+        } catch (JsonMappingException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (JsonProcessingException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return null;
+    }
+    
+}
+
